17bf3727ab501061c840cbfbf84e63f15ac65fb0
mskyberg <mskyberg@gmail.com>
2020-07-01 12:47:35 -0500

    added customer members and implemented display logic

--- Topic-2/class_definitions/customer.py
+++ Topic-2/class_definitions/customer.py
@@ -9,21 +9,21 @@ Purpose: Demonstrate the use of encapsulation by creating an customer class
 
 class Customer(object):
     """ Customer Class """
-    def __init__(self):
+    def __init__(self, c_id, lname, fname, phone, addr):
         """
         Constructor for Customer class
 
-        :param c_id:
-        :param lname:
-        :param fname:
-        :param phone:
-        :param addr:
+        :param c_id: Customer id
+        :param lname: Customer Last Name
+        :param fname: Customer First Name
+        :param phone: Customer Phone Number
+        :param addr: Customer address
         """
-        # customer_id - required: number
-        # last_name - required: string
-        # first_name - required: string
-        # phone_number - required: string
-        # address - required: string
+        self.customer_id = c_id
+        self.last_name = lname
+        self.first_name = fname
+        self.phone_number = phone
+        self.address = addr
         pass
 
     def display(self):
@@ -32,7 +32,15 @@ class Customer(object):
 
         :returns: string of customer information
         """
-        pass
+        # cleanup address
+        address_list = self.address.split(', ')
+        street = f'{address_list[0]}'
+        city_state = f'{address_list[1]}, {address_list[2]}'
+
+        # return final string
+        return f'{self.first_name} {self.last_name}, Customer#: ' \
+               f'{self.customer_id}\n{street}\n{city_state}\n' \
+               f'{self.phone_number}'
 
     def __str__(self):
         pass
