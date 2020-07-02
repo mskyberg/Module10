3681ec1df5e29cafdaa9718007c06f2e8599d086
mskyberg <mskyberg@gmail.com>
2020-07-01 19:09:08 -0500

    implmeneted add item and made driver work

--- Topic-2/class_definitions/invoice.py
+++ Topic-2/class_definitions/invoice.py
@@ -8,7 +8,7 @@ Purpose:
 
 
 class Invoice(object):
-    def __init__(self, inv_id, c_id, lname, fname, phone, addr, items={}):
+    def __init__(self, inv_id, c_id, addr, lname, fname, phone, items=dict()):
         """
         Invoice Class Constructor
 
@@ -28,17 +28,37 @@ class Invoice(object):
         self.address = addr
         self.items_with_price = items
 
-    def add_item(self):
+    def add_item(self, item):
         """
         add item to the total items dictionary
         
         :param item: the item key, value to add to items dictionary
         """
-        pass
+        self.items_with_price.update(item)
 
     def create_invoice(self):
         """
         Prints each item with price then a total with tax calculated
+        """
+        sales_tax = 0.06
+        item_sum = 0
+        inv = f'Invoice#: {self.invoice_id}\n'
+        for key, value in self.items_with_price.items():
+            item_sum += value
+            inv += f'{key}.....${value:.2f}\n'
+
+        tax = item_sum * sales_tax
+        inv += f'Tax.....${tax:.2f}\n'
+        inv += f'Total.....${tax + item_sum:.2f}'
+        print(inv)
+        # returning for unit testing purposes
+        return inv
+
+    def display(self):
+        """
+        display the invoice object information
+
+        :returns: string of invoice object information
         """
         pass
 
@@ -48,7 +68,13 @@ class Invoice(object):
                f'{self.address}\n{self.phone_number}\n{self.items_with_price}'
 
     def __repr__(self):
-        pass
+        return f'invoice_id = {self.invoice_id}\n' \
+               f'customer_id = {self.customer_id}\n' \
+               f'last_name = {self.last_name}\n' \
+               f'first_name = {self.first_name}\n' \
+               f'phone_number = {self.phone_number}\n' \
+               f'address = {self.address}\n' \
+               f'items_with_price = {self.items_with_price}\n'
 
 
 if __name__ == '__main__':
@@ -57,3 +83,6 @@ if __name__ == '__main__':
     invoice.add_item({'iPad': 799.99})
     invoice.add_item({'Surface': 999.99})
     invoice.create_invoice()
+
+    print(str(invoice))
+    print(repr(invoice))
--- Topic-2/test/test_invoice.py
+++ Topic-2/test/test_invoice.py
@@ -27,6 +27,34 @@ class MyTestCase(unittest.TestCase):
                               items={'iPad': 799.99})
         self.assertEqual(str(invoice), expected)
 
+    def test_invoice_creation_no_items(self):
+        expected = f'Invoice#: 201\n' \
+                   f'Tax.....$0.00\n' \
+                   f'Total.....$0.00'
+        invoice = inv.Invoice(inv_id=201,
+                              c_id=1234,
+                              lname='Skyberg',
+                              fname='Mike',
+                              phone='515-515-5551',
+                              addr='123 Heathrow Drive, Ames, Iowa')
+        self.assertEqual(invoice.create_invoice(), expected)
+
+    def test_invoice_creation(self):
+        items_dict = {'iPad': 799.99, 'Surface': 999.99}
+        expected = f'Invoice#: 42\n' \
+                   f'iPad.....$799.99\n' \
+                   f'Surface.....$999.99\n' \
+                   f'Tax.....$108.00\n' \
+                   f'Total.....$1907.98'
+        invoice = inv.Invoice(inv_id=42,
+                              c_id=1234,
+                              lname='Skyberg',
+                              fname='Mike',
+                              phone='515-515-5551',
+                              addr='123 Heathrow Drive, Ames, Iowa',
+                              items=items_dict)
+        self.assertEqual(invoice.create_invoice(), expected)
+
 
 if __name__ == '__main__':
     unittest.TestCase()
